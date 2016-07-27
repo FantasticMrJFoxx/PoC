c9ddee514f9e1bf180b12bbe4b06e589146de689
jaime alday <jalday@nearsoft.com>
2016-07-26 22:44:46 -0700

    Playing with Chrome driver capabilities, switches & flags

--- launch-browser.rb
+++ launch-browser.rb
@@ -1,15 +1,36 @@
 #!/usr/bin/env ruby
 
-# Code mainly taken from:
-# https://github.com/jnicklas/capybara#configuring-and-adding-drivers
+### about Chrome driver
+#>> arguments (switches)
+#' http://peter.sh/experiments/chromium-command-line-switches/
+#>> flags
+#' chrome://flags/
+#' chrome://local-state/
+#>> Capabilities
+#' https://sites.google.com/a/chromium.org/chromedriver/capabilities
 
 require 'capybara'
 require 'capybara/dsl'
 require 'pry'
+require 'selenium-webdriver'
 
 # overrides selenium's driver to use chrome browser
 Capybara.register_driver :selenium do |app|
-  Capybara::Selenium::Driver.new(app, :browser => :chrome)
+  caps = Selenium::WebDriver::Remote::Capabilities.chrome
+=begin
+  caps['chromeOptions'] = {}
+  caps['chromeOptions']['args'] = ['--incognito']
+  caps['chromeOptions']['localState'] = {
+    'protocol_handler': {
+      'excluded_schemes': {
+        'mailto': false,
+      }
+    }
+  }
+=end
+  Capybara::Selenium::Driver.new(app, {detach: false,
+                                       browser: :chrome,
+                                       desired_capabilities: caps})
 end
 
 # selecting the driver
